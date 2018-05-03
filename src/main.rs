fcd002f97b1e2a94b16490c8d0ad692b13bca585
AustinHaugerud <austin15328@hotmail.com>
2018-05-03 03:03:10 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -502,7 +502,7 @@ impl ServerCallbacks for ExampleServer {
     fn update(&mut self, context : &UpdateContext) {
         let current = Instant::now();
         if current.duration_since(self.last_update_time) > Duration::from_secs(1 ) {
-            println!("Update.");
+            println!("Update: {:?}", current);
             self.last_update_time = current;
 
             for (player_id, player) in self.players.iter_mut() {
